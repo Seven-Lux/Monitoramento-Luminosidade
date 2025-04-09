5e36272f95b340d669674768ec75a29bf03a57aa
Bryan-Anthony <bryan.amoraes@sptech.school>
2025-04-09 09:52:56 -0300

    Código Arduino Atualizado

--- CodArduinoIDE/CodArduinoIDE.ino
+++ CodArduinoIDE/CodArduinoIDE.ino
@@ -3,6 +3,11 @@ const int PINO_SENSOR_LDR = A1;
 
 // Declara uma variável e que ela ira armazenar os dados coletados pelo arduino.
 int valorLuminosidade;
+float tensao5;
+float resistorVoltagem;
+float ldrVoltagem;
+float ldrResistencia;
+float ldrLux;;
 
 // Cria uma função, ela declara a velocidade da comunicação com o computador.
 void setup() {
@@ -14,10 +19,37 @@ void setup() {
 void loop() {
   // Função criada para ler os valores que estão na porta setada da variável em "()".
   valorLuminosidade = analogRead(PINO_SENSOR_LDR);
+  
+  // converte para tensão
+  float tensao30 = valorLuminosidade * 5 / 1023.0;
+
+  // Resitência do LDR 30k
+  float rdr30 = (30000 * (5-tensao30) / tensao30);
+
+  // tensão 5k 
+  float tensao5 = 5 * (5000 * (5000 / (5000 - rdr30)));
+
+  // leitura analogica de 5k
+  int analogValue5k = tensao5 * 1023/ 5;
+
+// Resistência para conversão de lux
+  resistorVoltagem = (float)analogValue5k / 1023 * 5;
+
+  // voltage across the LDR is the 5V supply minus the 5k resistor voltage
+  ldrVoltagem = 5 - resistorVoltagem;
+
+  // resistencia 5k
+  ldrResistencia = ldrVoltagem / resistorVoltagem * 5030;
+  // converte pra luminância
+  ldrLux = 12518931 * pow(ldrResistencia, -1.405);
+
+  
   // Coloca o texto no monitor serial.
   Serial.print("Luminosidade: ");
+
   // pula linha após inserir o valor.
-  Serial.println(valorLuminosidade);
+  Serial.println(ldrLux);
+
   // Define o intervalo de tempo que virá uma nova informação do arduino ao meu computador.
   // pausando o programa por 2000 milissegundos (2seg).
   delay(2000);
