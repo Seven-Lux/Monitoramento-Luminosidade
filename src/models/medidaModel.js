4a77ecc4266b93dccadc7da95e1380d03be7cf83
JoaoVtC <joao.varaujo@sptech.school>
2025-06-03 22:12:59 -0300

    mudanças de lógica e sintaxe na captura em tempo real

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -16,7 +16,7 @@ function buscarUltimasMedidas(idCorredor, limite_linhas) {
                                 from dados
                                 where fkSensor = ${resposta[0].idSensor}
                                 group by day(dtHora)
-                                order by day(dtHora) desc limit ${limite_linhas} `
+                                order by day(dtHora) limit ${limite_linhas} `
             console.log("Executando a instrução SQL: \n" + instrucaoSql2);
             return database.executar(instrucaoSql2)
 
